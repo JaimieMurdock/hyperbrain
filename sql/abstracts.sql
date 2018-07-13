ae31bc32f7a6732a9a04a1054f3166403b8fc16e
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-07-13 03:12:51 -0400

    fixing WHERE clause

--- sql/abstracts.sql
+++ sql/abstracts.sql
@@ -3,8 +3,8 @@ FROM wos_dynamic_identifiers issn, wos_dynamic_identifiers doi, wos_abstract_par
 WHERE issn.id = t.id
     AND issn.id = abs.id
     AND t.title_type = 'item'
-    AND wdi.identifier_type = 'issn'
-    AND wdi.identifier_value IN (
+    AND issn.identifier_type = 'issn'
+    AND issn.identifier_value IN (
         '1863-2661', '1530-8898', '0022-3077', '0301-0511', '0094-9345', '1573-6792', '1433-0350', '1460-9568', '1662-5129', '1432-1106', '0028-3932'
     )
     AND issn.id = doi.id
