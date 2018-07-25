3f35ff32bb78a5a552b0ebc65735899a7216837d
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-07-25 16:45:04 -0400

    correcting column order

--- sql/graph.sql
+++ sql/graph.sql
@@ -9,7 +9,7 @@ WHERE article.id = issn.id
     AND (names.seq_no = '1' OR names.seq_no = article.name_count);
 */
 
-SELECT article.id, names.seq_no, names.wos_standard, article.sortdate, article.pubyear, citation.ref_id, citation.year
+SELECT names.wos_standard, names.seq_no, article.sortdate, article.id, article.pubyear, citation.ref_id, citation.year
 FROM wos_dynamic_identifiers issn, wos_summary_names names, wos_summary article, wos_references citation
 WHERE article.id = issn.id
     AND issn.identifier_type = 'issn'
