c86fa9c89ab7acf4c08f9bf74f86b80586a5beb0
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-07-26 21:32:30 -0400

    updating list of journals

--- docs/WoS-issues.md
+++ docs/WoS-issues.md
@@ -6,4 +6,12 @@
 
 - It is unclear whether journals are listed by print or online ISSNs
 
+- online vs print issns and duplicate data
+    - there are both issn and eissn for things, are they both listed together?
+
+# postgres issues
+
 - some citation identifiers appear to have return characters or there is an 80-char truncation in `psql` output
+
+- cannot create temporary tables
+- cannot COPY data to or from CSV files.
--- sql/graph.sql
+++ sql/graph.sql
@@ -9,16 +9,15 @@ WHERE article.id = issn.id
     AND (names.seq_no = '1' OR names.seq_no = article.name_count);
 */
 
-CREATE TEMPORARY TABLE issns(issn VARCHAR, journal_name VARCHAR)
-
-COPY isssns FROM '/home/jammurdo/Karst/hyperbrain/sql/issns.tsv' DELIMITER E'\t';
+-- awk '{print $1}' issns.tsv | tr '\n' '\r' | sed "s/\r/','/g"
 
 SELECT names.wos_standard, names.seq_no, article.sortdate, article.id, article.pubyear, citation.ref_id, citation.year
-FROM wos_dynamic_identifiers issn, wos_summary_names names, wos_summary article, wos_references citation, issns
+FROM wos_dynamic_identifiers issn, wos_summary_names names, wos_summary article, wos_references citation
 WHERE article.id = issn.id
     AND issn.identifier_type = 'issn'
-    AND issn.identifier_value = issns.issn
+    AND issn.identifier_value IN (
+        '1471-003X','0006-8950','1758-8928','0166-2236','1863-2653','0898-929X','0022-3077','0301-0511','0896-0267','0256-7040','0270-6474','0896-6273','1097-6256','1047-3211','0006-8950','1053-8119','1065-9471','0010-9452','0953-816X','1359-5962','0014-4819','0028-3932','0147-006X'
+    )
     AND article.id = names.id
     AND (names.seq_no = '1' OR names.seq_no = article.name_count)
-    AND article.id = citation.id
-LIMIT 10;
+    AND article.id = citation.id;
