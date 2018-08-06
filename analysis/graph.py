2bab2156e1ce69f5d63711953f3b534444ade0db
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-08-06 16:44:25 -0400

    analysis graph update - tab separation

--- analysis/graph.py
+++ analysis/graph.py
@@ -23,6 +23,7 @@ if __name__ == '__main__':
     graph = build_graph(args.graphfile)
     for author, year_readings in graph.items():
         years = sorted(year_readings.keys())
+<<<<<<< Updated upstream
         if len(years) > 3:
             print(author)
             encountered = set()
@@ -30,4 +31,15 @@ if __name__ == '__main__':
                 print(year, len(year_readings[year]),
                 len(year_readings[year].difference(encountered)),
                 len(encountered))
+=======
+        if len(years) > 1:
+            print(author)
+            encountered = set()
+            for year in years:
+                print(
+                    year, 
+                    len(year_readings[year]),
+                    len(encountered.difference(year_readings[year])),
+                    len(encountered), sep='\t')
+>>>>>>> Stashed changes
                 encountered.update(year_readings[year])
