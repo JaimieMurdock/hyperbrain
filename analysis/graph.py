36e7a5184080ab74020008a6e0cf59bdce9a9d24
Jaimie Murdock <jaimie.murdock@gmail.com>
2018-08-11 16:55:08 -0400

    adding cumulative flag

--- analysis/graph.py
+++ analysis/graph.py
@@ -53,7 +53,9 @@ def build_encounters(year_readings):
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('graphfile')
-    parser.add_argument('conifg')
+    parser.add_argument('config')
+    parser.add_argument('-k', type=int)
+    parser.add_argument('-c', '--cumulative', action='store_true')
     args = parser.parse_args()
 
     te = topicexplorer.from_config(args.config)
@@ -67,14 +69,29 @@ if __name__ == '__main__':
         logging.debug(f"PROCESSING {author}")
         assert readings[author].keys() == writings[author].keys()
 
+        if args.cumulative:
+            r = set()
+            w = set()
         for year in sorted(readings[author].keys()):
-            reading_topics = te[20].doc_topic_matrix(readings[author][year]).mean(axis=0)
-            writing_topics = te[20].doc_topic_matrix(writings[author][year]).mean(axis=0)
+            if args.cumulative:
+                r.update(article for article in readings[author][year] 
+                         if article in te.corpus)
+                w.update(article for article in writings[author][year] 
+                         if article in te.corpus)
+            else:
+                r = [article for article in readings[author][year] 
+                         if article in te.corpus]
+                w = [article for article in writings[author][year] 
+                         if article in te.corpus]
+            if r and w:
+                reading_topics = te[args.k].doc_topic_matrix(r).mean(axis=0)
+                writing_topics = te[args.k].doc_topic_matrix(w).mean(axis=0)
             
-            write_from_read = KL_div(reading_topics, writing_topics)
-            read_from_write = KL_div(writing_topics, reading_topics)
+                write_from_read = KL_div(reading_topics, writing_topics)
+                read_from_write = KL_div(writing_topics, reading_topics)
 
-            print(author, year, write_from_read, read_from_write, sep='\t')
+                print(author, year, write_from_read, read_from_write,
+                      len(r), len(w), sep='\t')
 
 """
 for each author
